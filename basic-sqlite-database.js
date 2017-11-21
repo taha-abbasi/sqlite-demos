bebb8dbecfe604dd48cba68416c41cdd6ff454d1
Taha Abbasi <taha@mywebnapp.com>
2017-11-21 00:35:29 -0500

    Added model and db

---
+++ basic-sqlite-database.js
@@ -0,0 +1,28 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING
+    },
+    completed: {
+        type: Sequelize.BOOLEAN
+    }
+});
+
+sequelize.sync().then(function () {
+    console.log('Everything is synced');
+
+
+    Todo.create({
+        description: 'Walking my dog',
+        completed: false
+    }).then(function (todo) {
+        console.log('Finished');
+        console.log(todo);
+    });
+
+});
